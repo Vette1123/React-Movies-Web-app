ba4071efbf6e1257cfd703f17557754edf616bea
MohamedGado <boogado@yahoo.com>
2022-05-08 07:30:41 +0200

    Improving the app functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import 'swiper/swiper.min.css';
-import './assets/boxicons-2.0.7/css/boxicons.min.css';
-import './App.scss';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
 
-import { BrowserRouter, Route } from 'react-router-dom';
-
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-
-import Routes from './config/Routes';
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Routees from "./config/Routees";
+import Header from "./components/header/Header";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Route render={props => (
-                <>
-                    <Header {...props}/>
-                    <Routes/>
-                    <Footer/>
-                </>
-            )}/>
-        </BrowserRouter>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routees />
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
