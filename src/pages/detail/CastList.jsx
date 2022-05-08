8076a9fa6f02d3915ddb8bcff8a268c5d6fcb777
MohamedGado <boogado@yahoo.com>
2022-05-08 08:46:02 +0200

    Improving loading time

--- src/pages/detail/CastList.jsx
+++ src/pages/detail/CastList.jsx
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 
-import tmdbApi from '../../api/tmdbApi';
-import apiConfig from '../../api/apiConfig';
+import tmdbApi from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
+import Spinner from "../../components/spinner/Spinner";
 
-const CastList = props => {
+const CastList = (props) => {
+  const { category } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [casts, setCasts] = useState([]);
 
-    const {category} = useParams();
-
-    const [casts, setCasts] = useState([]);
-
-    useEffect(() => {
-        const getCredits = async () => {
-            const res = await tmdbApi.credits(category, props.id);
-            setCasts(res.cast.slice(0, 5));
-        }
-        getCredits();
-    }, [category, props.id]);
-    return (
-        <div className="casts">
-            {
-                casts.map((item, i) => (
-                    <div key={i} className="casts__item">
-                        <div className="casts__item__img" style={{backgroundImage: `url(${apiConfig.w500Image(item.profile_path)})`}}></div>
-                        <p className="casts__item__name">{item.name}</p>
-                    </div>
-                ))
-            }
+  useEffect(() => {
+    const getCredits = async () => {
+      const res = await tmdbApi.credits(category, props.id);
+      setCasts(res.cast.slice(0, 5));
+    };
+    getCredits();
+    setLoading(false);
+  }, [category, props.id]);
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <div className="casts">
+      {casts.map((item, i) => (
+        <div key={i} className="casts__item">
+          <div
+            className="casts__item__img"
+            style={{
+              backgroundImage: `url(${apiConfig.w500Image(item.profile_path)})`,
+            }}
+          ></div>
+          <p className="casts__item__name">{item.name}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default CastList;
